28891b41df8bf7cb10a98fe8d4a6094441e5377a
culi <culitif@tuta.io>
2021-11-11 13:23:27 -0800

    move district data

--- lib/services/vagov2021/absentee.ts
+++ lib/services/vagov2021/absentee.ts
@@ -18,29 +18,6 @@ import { absenteeData } from './types';
 }
 */
 
-const districts = {
-  1: {
-    drawDate: '2017-01-03',
-    wikipedia: 'https://en.wikipedia.org/wiki/Virginia%27s_1st_congressional_district',
-    representative: 'Rob Wittman',
-    representativeParty: 'Republican',
-    cookPVI: 'R+6',
-    urbanDistribution: 0.6954,
-    ruralDistribution: 0.3046,
-    population: 824942,
-    populationYear: 2019,
-    medianHouseholdIncome: 90181,
-    ethnicity: {
-      white: 0.7201,
-      black: 0.1586,
-      hispanic: 0.0935,
-      asian: 0.0357,
-      native: 0.0043
-    },
-    cities: [ 'Fredericksburg' ]
-  }
-}
-
 const mungeAbsenteeData = (data: any): absenteeData => ({
   ageRange: data.AgeRange,
   congressionalDistrict: Number(data.CongressionalDistrict),
@@ -75,4 +52,4 @@ const fetchData = async () => {
 }
 
 export default fetchData
-export { districts, API_URL as URL }
+export { API_URL as URL }
---
+++ lib/services/vagov2021/data.ts
@@ -0,0 +1,32 @@
+
+const districts = {
+  1: {
+    drawDate: '2017-01-03',
+    wikipedia: 'https://en.wikipedia.org/wiki/Virginia%27s_1st_congressional_district',
+    representative: 'Rob Wittman',
+    representativeParty: 'Republican',
+    cookPVI: 'R+6',
+    urbanDistribution: 0.6954,
+    ruralDistribution: 0.3046,
+    population: 824942,
+    populationYear: 2019,
+    medianHouseholdIncome: 90181,
+    ethnicity: {
+      white: 0.7201,
+      black: 0.1586,
+      hispanic: 0.0935,
+      asian: 0.0357,
+      native: 0.0043
+    },
+    cities: [ 'Fredericksburg' ]
+  }
+}
+
+const localities = {
+
+}
+
+export {
+  districts,
+  localities
+}
